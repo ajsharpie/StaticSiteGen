2325f88cea0a91b0bfd8258a37deb7412aa26a15
ajsharpie <a.j.sharpie@gmail.com>
2025-02-02 15:47:54 -0700

    pre-testing state

--- src/main.py
+++ src/main.py
@@ -4,18 +4,9 @@ from block_to_html import markdown_to_html_node
 from textnode import TextNode
 
 def main():
-    file = open('content/index.md')
-    mdtext = file.read()
-    file.close()
 
-    
-    os.mknod('publictest/test1.py')
-    file = open('publictest/test1.py')
-    file.write('thisisthetext')
-    file.close()
-    
-    # copy_to_path('static', 'public')
-    # generate_page('content/index.md', 'template.html', 'public/index.html')
+    copy_to_path('static', 'publictest')
+    generate_page('content/index.md', 'template.html', 'publictest/index.html')
     pass
         
 def copy_to_path(src, dest):
@@ -37,7 +28,7 @@ def extract_title(markdown):
 def generate_page(from_path, template_path, dest_path):
     print(f'generating page: from {from_path}, to {dest_path}, using {template_path}')
     
-    if os.file.exists(from_path) and os.file.exists(template_path) and os.file.exists(dest_path):
+    if os.file.exists(from_path) and os.file.exists(template_path):
         file = open(from_path)
         mdtext = file.read()
         file.close()
@@ -57,15 +48,10 @@ def generate_page(from_path, template_path, dest_path):
         if not os.path.exists(dirname):
             os.makedirs(dirname)
         
-        file = open()
-        
-        
-        
-        
-        
-        
-        
-        
+        file = open(dest_path, 'w')
+        file.write(template)
+        file.close()
+           
         
         
     else:
